9b28652cd801259b135fae80155ad51c641efb1a
Matt <rajal038@umn.edu>
2025-05-09 12:18:29 -0500

    use path to model

--- scripts/merge_lora.sh
+++ scripts/merge_lora.sh
@@ -6,8 +6,8 @@ MODEL_NAME="Qwen/Qwen2.5-VL-7B-Instruct"
 export PYTHONPATH=src:$PYTHONPATH
 
 python src/merge_lora_weights.py \
-  --model-path /home/workspace/Qwen2-VL-Finetune/output/testing_lora \
+  --model-path ~/Qwen2-VL-Finetune/output/testing_lora \
   --model-base $MODEL_NAME \
-  --save-model-path /home/workspace/Qwen2-VL-Finetune/output/merge_test \
+  --save-model-path ~/Qwen2-VL-Finetune/output/merge_test \
   --safe-serialization
 
